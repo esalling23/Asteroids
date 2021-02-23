7266bd5a3230923a1f4757c76369082a9d598d83
esalling23 <esalling23@gmail.com>
2021-02-22 19:10:44 -0500

    Asteroids split on half-health
    Modifies Asteroid to have half-life state
    If Asteroid is at half health, instantiate a duplicate & shrink size in half

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -7,9 +7,13 @@ public class Asteroid : MonoBehaviour
     const float MinImpulseForce = 3000f;
     const float MaxInpulseForce = 5000f;
 
+    bool halfLife = false;
     int health = 100;
     int maxHealth;
     SpriteRenderer spriteRenderer;
+
+    [SerializeField]
+    GameObject asteroid;
 
     // Start is called before the first frame update
     void Start()
@@ -44,6 +48,21 @@ public class Asteroid : MonoBehaviour
         GetComponent<Rigidbody2D>().AddForce(moveDirection * magnitude, ForceMode2D.Impulse);
     }
 
+    public void HalfLife()
+    {
+        halfLife = true;
+
+        if (health > maxHealth / 2)
+        {
+            health = maxHealth / 2;
+        }
+
+        Vector3 halfSizeScale = transform.localScale;
+        halfSizeScale.x /= 2;
+        halfSizeScale.y /= 2;
+        transform.localScale = halfSizeScale;
+    }
+
     public void TakeDamage(int damage)
     {
         health -= damage;
@@ -57,5 +76,17 @@ public class Asteroid : MonoBehaviour
         {
             Destroy(gameObject);
         }
+        else if (maxHealth / health >= 2 && !halfLife)
+        {
+            // If we hit half-health, go half size & spawn another half-size
+            HalfLife();
+
+            Asteroid duplicate = Instantiate(asteroid, transform.position, Quaternion.identity).GetComponent<Asteroid>();
+            Direction direction = (Direction)Random.Range(0, 4);
+            print(direction);
+            duplicate.Initialize(direction);
+        }
+
+        
     }
 }
